4424682152e382d714fc6c541e69d10d5a16128f
ksantaro <ksantaro@uci.edu>
2018-12-19 13:12:52 -0800

    added the admin portal sidebar functions and styles

--- elevate/frontend/src/components/AdminPortal.js
+++ elevate/frontend/src/components/AdminPortal.js
@@ -5,60 +5,107 @@ const routes = [ //routes will also become props
   {
     path: "/frontend/admin/0",
     exact: true,
-    sidebar: () => <div>dashboard component</div>,
+    sidebar: () => 
+      <span>
+        <h2>Admin</h2> {/* Replace with admin username later */}
+              <ul>
+                <li> {/* The link tags will be become props also change id
+                         Also may need to add a /id/bussiness/id route to
+                         instruct to pull data and fill appropriate fields
+                       */}
+                  <span className="selected-indicator"></span>
+                  <Link className="selected" to="/frontend/admin/0/dashboard">Dashboard</Link>
+                </li>
+                <li>
+                  <Link to="/frontend/admin/0/add-business">Add Business</Link>
+                </li>
+                <li>
+                  <a href="/frontend/admin-login" >Logout</a>
+                  {/* <Link to="/frontend/admin-login">Logout</Link> */}
+                </li>
+              </ul>
+      </span>,
     main: () => <h2>dashboard</h2>
   },
   {
     path: "/frontend/admin/0/dashboard",
-    sidebar: () => <div>dashboard component</div>,
+    sidebar: () => 
+      <span>
+      <h2>Admin</h2> 
+            <ul>
+              <li> 
+                <span className="selected-indicator"></span>
+                <Link className="selected" to="/frontend/admin/0/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <Link to="/frontend/admin/0/add-business">Add Business</Link>
+              </li>
+              <li>
+                <a href="/frontend/admin-login" >Logout</a>
+              </li>
+            </ul>
+      </span>,
     main: () => <h2>dashboard</h2>
   },
   {
     path: "/frontend/admin/0/add-business",
-    sidebar: () => <div>add business component</div>,
+    sidebar: () => 
+      <span>
+      <h2>Admin</h2>
+            <ul>
+              <li> 
+                <Link to="/frontend/admin/0/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <span className="selected-indicator"></span>
+                <Link className="selected" to="/frontend/admin/0/add-business">Add Business</Link>
+              </li>
+              <li>
+                <a href="/frontend/admin-login" >Logout</a>
+              </li>
+            </ul>
+      </span>,
     main: () => <h2>add business</h2>
   },
   {
     path: "/frontend/admin/0/NMIsetup-1",
-    sidebar: () => <div>NMI setup step 1 component</div>,
+    sidebar: () => 
+      <div>NMI setup step 1 component</div>,
     main: () => <h2>nmi setup</h2>
   },
   {
     path: "/frontend/admin/0/NMIsetup-2",
-    sidebar: () => <div>NMI setup step 2 component</div>,
+    sidebar: () => 
+      <div>NMI setup step 2 component</div>,
     main: () => <h2>nmi setup</h2>
   },
 ];
 
 class AdminPortal extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        dashboardSelected: true,
+        addBussinessSelected: false,
+      }
+    }
+  
     render() {
       return (
         <Router>
-          <div style={{ display: "flex" }}>
+          <div className="row portal">
             <div
-              style={{
-                padding: "10px",
-                width: "40%",
-                background: "#f0f0f0"
-              }}
+              // className="sidebar" 
+              className="col-2 sidebar"
+              // style={{
+              //   padding: "10px",
+              //   width: "40%",
+              //   background: "#f0f0f0"
+              // }}
             >
-              <ul style={{ listStyleType: "none", padding: 0 }}>
-                <li> {/* The link tags will be become props also change id
-                         Also may need to add a /id/bussiness/id route to
-                         instruct to pull data and fill appropriate fields
-                       */}
-                  <Link to="/frontend/admin/0/dashboard">dashboard</Link>
-                </li>
-                <li>
-                  <Link to="/frontend/admin/0/add-business">add business</Link>
-                </li>
-                <li>
-                  <a href="#" >Logout</a>
-                  
-                </li>
-              </ul>
+              {/* Used to be the normal div */}
 
-              {/* {routes.map((route, index) => (
+              {routes.map((route, index) => ( // For a subheading wont need it
                 // You can render a <Route> in as many places
                 // as you want in your app. It will render along
                 // with any other <Route>s that also match the URL.
@@ -72,10 +119,13 @@ class AdminPortal extends Component {
                   exact={route.exact}
                   component={route.sidebar}
                 />
-              ))} */}
+              ))}                
             </div>
 
-            <div style={{ flex: 1, padding: "10px" }}>
+            <div 
+              // className="right-container"
+              className="col-10 right-container"
+            >
               {routes.map((route, index) => (
                 // Render more <Route>s with the same paths as
                 // above, but different components this time.
