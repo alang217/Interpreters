9697d1eb445069888fe245e630d1a2551d75fd99
ksantaro <ksantaro@uci.edu>
2018-12-20 14:25:02 -0800

    portal component has been made and Admin Portal uses it now

--- elevate/frontend/src/components/AdminPortal.js
+++ elevate/frontend/src/components/AdminPortal.js
@@ -1,144 +1,75 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Portal from "./Portal.js";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"; //delete after
 
-const routes = [ //routes will also become props
-  {
-    path: "/frontend/admin/0",
-    exact: true,
-    sidebar: () => 
-      <span>
-        <h2>Admin</h2> {/* Replace with admin username later */}
-              <ul>
-                <li> {/* The link tags will be become props also change id
-                         Also may need to add a /id/bussiness/id route to
-                         instruct to pull data and fill appropriate fields
-                       */}
-                  <span className="selected-indicator"></span>
-                  <Link className="selected" to="/frontend/admin/0/dashboard">Dashboard</Link>
-                </li>
-                <li>
-                  <Link to="/frontend/admin/0/add-business">Add Business</Link>
-                </li>
-                <li>
-                  <a href="/frontend/admin-login" >Logout</a>
-                  {/* <Link to="/frontend/admin-login">Logout</Link> */}
-                </li>
-              </ul>
-      </span>,
-    main: () => <h2>dashboard</h2>
-  },
-  {
-    path: "/frontend/admin/0/dashboard",
-    sidebar: () => 
-      <span>
-      <h2>Admin</h2> 
-            <ul>
-              <li> 
-                <span className="selected-indicator"></span>
-                <Link className="selected" to="/frontend/admin/0/dashboard">Dashboard</Link>
-              </li>
-              <li>
-                <Link to="/frontend/admin/0/add-business">Add Business</Link>
-              </li>
-              <li>
-                <a href="/frontend/admin-login" >Logout</a>
-              </li>
-            </ul>
-      </span>,
-    main: () => <h1>dashboard</h1>
-  },
-  {
-    path: "/frontend/admin/0/add-business",
-    sidebar: () => 
-      <span>
-      <h2>Admin</h2>
-            <ul>
-              <li> 
-                <Link to="/frontend/admin/0/dashboard">Dashboard</Link>
-              </li>
-              <li>
-                <span className="selected-indicator"></span>
-                <Link className="selected" to="/frontend/admin/0/add-business">Add Business</Link>
-              </li>
-              <li>
-                <a href="/frontend/admin-login" >Logout</a>
-              </li>
-            </ul>
-      </span>,
-    main: () => <h1>add business</h1>
-  },
-  {
-    path: "/frontend/admin/0/NMIsetup-1",
-    sidebar: () => 
-      <div>NMI setup step 1 component</div>,
-    main: () => <h1>nmi setup</h1>
-  },
-  {
-    path: "/frontend/admin/0/NMIsetup-2",
-    sidebar: () => 
-      <div>NMI setup step 2 component</div>,
-    main: () => <h1>nmi setup</h1>
-  },
-];
 
 class AdminPortal extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        dashboardSelected: true,
-        addBussinessSelected: false,
+        username: "",
+        id: null,
       }
+    }
+
+    componentDidMount() {
+      //if you not logged in kick out of page or not correct id
+
+        this.setState({ // for now until we connect with backend
+          id: this.props.match.params.id,
+          username: "Admin"
+        })
+        // params.id
     }
   
     render() {
+      /*  routes is a list of route objects
+
+          path: the link needed to get to the specific route
+          name: the name to be displayed on the sidebar
+              if empty, the route will not appear on the sidebar
+          selected: shows which tab to highlight if currently on this route
+          exact: if true the path has to be exactly as shown.
+          main: the main component to render to the right of the sidebar
+      */
+     let routes = [];
+      if (this.state.id !== null) {
+        routes = [ //routes will also become props
+          {
+            path: `/frontend/admin/${this.state.id}`,
+            name: "",
+            selected: "Dashboard",
+            exact: true,
+            main: () => <h1>Dashboard</h1>
+          },
+          {
+            path: `/frontend/admin/${this.state.id}/dashboard`,
+            name: "Dashboard",
+            selected: "Dashboard",
+            main: () => <h1>dashboard</h1>
+          },
+          {
+            path: `/frontend/admin/${this.state.id}/add-business`,
+            name: "Add Business",
+            selected: "Add Business",
+            main: () => <h1>add business <Link to={`/frontend/admin/${this.state.id}/NMIsetup-1`}>To NMI</Link></h1>
+          },
+          {
+            path: `/frontend/admin/${this.state.id}/NMIsetup-1`,
+            name: "",
+            selected: "Add Business",
+            main: () => <h1>nmi setup 1<Link to={`/frontend/admin/${this.state.id}/NMIsetup-2`}>To NMI 2</Link></h1>
+          },
+          {
+            path: `/frontend/admin/${this.state.id}/NMIsetup-2`,
+            name: "",
+            selected: "Add Business",
+            main: () => <h1>nmi setup 2</h1>
+          },
+        ];
+      }
       return (
-        <Router>
-          <div className="row portal">
-            <div
-              // className="sidebar" 
-              className="col-2 sidebar"
-              // style={{
-              //   padding: "10px",
-              //   width: "40%",
-              //   background: "#f0f0f0"
-              // }}
-            >
-              {/* Used to be the normal div */}
-
-              {routes.map((route, index) => ( // For a subheading wont need it
-                // You can render a <Route> in as many places
-                // as you want in your app. It will render along
-                // with any other <Route>s that also match the URL.
-                // So, a sidebar or breadcrumbs or anything else
-                // that requires you to render multiple things
-                // in multiple places at the same URL is nothing
-                // more than multiple <Route>s.
-                <Route
-                  key={index}
-                  path={route.path}
-                  exact={route.exact}
-                  component={route.sidebar}
-                />
-              ))}                
-            </div>
-
-            <div 
-              // className="right-container"
-              className="col-10 right-container"
-            >
-              {routes.map((route, index) => (
-                // Render more <Route>s with the same paths as
-                // above, but different components this time.
-                <Route
-                  key={index}
-                  path={route.path}
-                  exact={route.exact}
-                  component={route.main}
-                />
-              ))}
-            </div>
-          </div>
-        </Router>
+          this.state.id !== null ? <Portal routes={routes} username={this.state.username}/> : <div></div>
       );
     }
   }
--- elevate/frontend/src/components/Portal.js
+++ elevate/frontend/src/components/Portal.js
@@ -1,11 +1,89 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class Portal extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        dashboardSelected: true,
+        addBussinessSelected: false,
+      }
+    }
+  
     render() {
+
+      const sidebarOptions = []; // get tabs to render
+      const sidebarPaths = []; // get paths the tabs point to
+      for (let i = 0; i < this.props.routes.length; i++) {
+        let routeName = this.props.routes[i].name;
+        let routePath = this.props.routes[i].path;
+        if (routeName !== "") {          
+          sidebarOptions.push(routeName);
+          sidebarPaths.push(routePath);
+        }
+      }
+      
+      // create formatting for the tabs
+      for (let i = 0; i < this.props.routes.length; i++) { // for each route
+        const jsxSidebar = [];
+        let route = this.props.routes[i];
+        for (let j = 0; j < sidebarOptions.length; j++) {
+          if (sidebarOptions[j] === route.selected) { // for each tab in the sidebar
+            jsxSidebar.push(
+              <li key={"sidebar" + i + j}> 
+                <span className="selected-indicator"></span>
+                <Link className="selected" to={sidebarPaths[j]}>{sidebarOptions[j]}</Link>
+              </li>
+            )
+          } else {
+            jsxSidebar.push(
+              <li key={"sidebar" + i + j}> 
+                <Link to={sidebarPaths[j]}>{sidebarOptions[j]}</Link>
+              </li>
+            )
+          }
+        }
+        jsxSidebar.push(
+          <li>
+            <a href="/frontend/admin-login" >Logout</a>
+          </li>
+          );
+        console.log(jsxSidebar)
+        this.props.routes[i].sidebar = () =>
+          <span>
+            <h2>{this.props.username}</h2> 
+             <ul>
+               {jsxSidebar}
+             </ul>
+          </span>
+      }
+
       return (
-        <div className="Portal">
-          this is the Portal
-        </div>
+        <Router>
+          <div className="row portal">
+            <div className="col-2 sidebar">
+              {this.props.routes.map((route, index) => ( // Renders appropriate sidebar
+                <Route
+                  key={index}
+                  path={route.path}
+                  exact={route.exact}
+                  component={route.sidebar}
+                />
+              ))}                
+            </div>
+
+            <div className="col-10 right-container">
+              {this.props.routes.map((route, index) => ( // Renders main component (right of sidebar)
+                <Route
+                  key={index}
+                  path={route.path}
+                  exact={route.exact}
+                  component={route.main}
+                />
+              ))}
+            </div>
+          </div>
+        </Router>
       );
     }
   }
